b21e78ee5af2db953de9b7935c8309dab56811ac
David <davidrums65@gmail.com>
2020-06-16 12:36:04 +0100

    Updates

---
+++ precache-manifest.823d63d9c9fb1d1fd2d39f45caa568eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190d9e51d54ff8695b872dff8aef8c25",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4c9e4d2b985f102803ae",
+    "url": "/monsters-rolodex/static/css/main.787e6e97.chunk.css"
+  },
+  {
+    "revision": "4487f1027138ab2c18e6",
+    "url": "/monsters-rolodex/static/js/2.48f0ea15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.48f0ea15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c9e4d2b985f102803ae",
+    "url": "/monsters-rolodex/static/js/main.f6028e33.chunk.js"
+  },
+  {
+    "revision": "d22463cfba6f2ed35e5d",
+    "url": "/monsters-rolodex/static/js/runtime-main.4fb94593.js"
+  }
+]);
